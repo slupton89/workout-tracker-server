304a573242235acdb565db6a8a86896e1259fbbd
Shane Lupton <slupton89@gmail.com>
2018-10-24 11:01:52 -0600

    updated config

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 module.exports = {
       PORT: process.env.PORT || 8080,
       CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-      DATABASE_URL: process.env.DATABASE_URL || 'mongodb://dev:devPassword1@ds045107.mlab.com:45107/workout-tracker-db',
-      TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://dev:devPassword1@ds055699.mlab.com:55699/workout-tracker-test-db',
+      DATABASE_URL: process.env.DATABASE_URL || 'http://localhost:3000',
+      TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'http://localhost:3000',
       JWT_SECRET: process.env.JWT_SECRET || 'SECRET',
       JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
