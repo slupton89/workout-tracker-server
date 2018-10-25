e34c70ad79a7b82ccc223383770bd5c49c6a1f37
Shane Lupton <slupton89@gmail.com>
2018-10-25 08:17:05 -0600

    update

--- dbmodels/User.js
+++ dbmodels/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   age: { type: Number, required: false },
-  height: { type: Number, required: false },
+  height: { type: String, required: false },
   weight: { type: Number, required: false },
 });
 
